465e10dba4c2c63fcaa7ee116e38e6251caebd8b
John Canty <jtcanty117@berkeley.edu>
2017-02-08 00:46:31 -0800

    Update

--- TwoColor/SubFns/PR_utrack_2color_v3.m
+++ TwoColor/SubFns/PR_utrack_2color_v3.m
@@ -20,8 +20,8 @@ in = load (inpath);
 disp('splitting traces');
 limit = max(in(:,end));
 lower = min(in(:,end));
-traces = [];
-for (i=lower:limit)
+traces = cell(1,limit-lower+1);
+for i=lower:limit
     selector = logical(in(:,end) == i);
     if ~isempty(in(selector,3:4))
         traces{i-lower+1} = in(selector,3:4);
@@ -34,7 +34,7 @@ disp(['split into ' num2str(limit) ' traces.']);
 %output: Aligned and centered traces for each two-color walker. Apply the
 %same rotation matrix to each pair of channels for each walker.
 traces = align_and_center(traces);
-for (i=1:limit)
+for i=1:limit
     if ~isempty(traces{i})
         traces{i} = traces{i}.*pixsize;       %convert to nm from pixels
     end    
@@ -44,7 +44,7 @@ end
 %user picks a start and finish with the mouse, or can reject trace entirely
 
 do = 0;
-while (~do)
+while ~do
     
     %prompt = 'Finished pre-processing. Review traces? [Y or N]';
     %review = input(prompt,'s'); %NOTE: Turned off
@@ -74,14 +74,16 @@ sig_all = sig;
 end
 
 function [traces_out,trace_id,sig_all] = review_traces(traces_in)    %go through each trace and ask for some rudimentary edits/modifications
-counter_2 = 0;
+j = 0;
 figure;
-for (i=1:length(traces_in))  
-    sig_on = std(abs(traces_in{i}(:,1)-smooth(traces_in{i}(:,1),10)));
-    if sig_on<3
-        sig_on
+for i=1:2:length(traces_in)  
+    sig_ch1 = std(abs(traces_in{i}(:,1)-smooth(traces_in{i}(:,1),10)));
+    sig_ch2 = std(abs(traces_in{i+1}(:,1)-smooth(traces_in{i+1}(:,1),10)));
+    if sig_ch1<3 && sig_ch2<3
         plot(traces_in{i}(:,1));
-        prompt = ['Trace ' num2str(i) ': keep (k), mod(m), sep(s), del(d), break(b) [k]'];
+        hold on
+        plot(traces_in{i+1}(:,1));
+        prompt = ['Two-color traces ' num2str(i) ' & ' num2str(i+1) ': keep (k), mod(m), sep(s), ig(i), break(b) '];
         review = input(prompt,'s');
         if ( strcmp(review,'m') )       %modify by clicking on end and beginning of the trace
             endpoint = length(traces_in{i}(:,1));
@@ -95,11 +97,11 @@ for (i=1:length(traces_in))
                 end
             end
             disp(['setting end, start to ' num2str(endpoint) ',' num2str(startpoint) ]);
-            counter_2  = counter_2 + 1;
-            disp(['count is ' num2str(counter_2)])
-            traces_out{counter_2} = traces_in{i}(startpoint:endpoint,:);
-            trace_id(counter_2,1) = i;
-            sig_all(counter_2,1) = sig_on;
+            j  = j + 2;
+            disp(['count is ' num2str(j)])
+            traces_out{j} = traces_in{i}(startpoint:endpoint,:);
+            trace_id(j,1) = i;
+            sig_all(j,1) = sig_ch1;
         elseif strcmp(review,'s')               %split the traces up as user inputs
             x = ginput(Inf);                    %get a bunch of points from ginput, which can be open-ended
             endpoint = length(traces_in{i});
@@ -109,32 +111,34 @@ for (i=1:length(traces_in))
                 pt_1 = round(x(j-1,1));
                 pt_2 = round(x(j,1));
                 if (pt_1 > pt_2)
-                    counter_2 = counter_2 + 1;
-                    disp(['count is ' num2str(counter_2)])
-                    traces_out{counter_2} = traces_in{i}(pt_2:pt_1,:);
-                    trace_id(counter_2,1) = i;
-                    sig_all(counter_2,1) = sig_on;
+                    j = j + 1;
+                    disp(['count is ' num2str(j)])
+                    traces_out{j} = traces_in{i}(pt_2:pt_1,:);
+                    trace_id(j,1) = i;
+                    sig_all(j,1) = sig_ch1;
                     disp (['made sub trace from' num2str(pt_2) ' to ' num2str(pt_1) '.']);
                 elseif (pt_2 > pt_1)
-                    counter_2 = counter_2 + 1;
-                    traces_out{counter_2} = traces_in{i}(pt_1:pt_2,:);
-                    trace_id(counter_2,1) = i;
-                    sig_all(counter_2,1) = sig_on;
+                    j = j + 1;
+                    traces_out{j} = traces_in{i}(pt_1:pt_2,:);
+                    trace_id(j,1) = i;
+                    sig_all(j,1) = sig_ch1;
                     disp (['made sub trace from' num2str(pt_2) ' to ' num2str(pt_1) '.']);
                 end
             end
-        elseif strcmp(review,'d');        %don't write trace out
-            disp(['deleting trace' num2str(i)]);
-            %traces_in{i} = [];
-        elseif strcmp(review,'b');          %break the loop and continue with UTP
+        elseif strcmp(review,'i');        
+            disp(['ignoring traces' num2str(i) ' & ' num2str(i+1)]);
+        elseif strcmp(review,'b');        
             break
         else
-            disp(['keeping trace' num2str(i)]);
-            counter_2 = counter_2 + 1;
-            disp(['count is ' num2str(counter_2)])
-            traces_out{counter_2} = traces_in{i};
-            trace_id(counter_2,1) = i;
-            sig_all(counter_2,1) = sig_on;
+            disp(['keeping trace' num2str(i) ' & ' num2str(i+1)]);
+            j = j + 2;
+            disp(['count is ' num2str(j)])
+            traces_out{j-1} = traces_in{i};
+            traces_out{j} = traces_in{i+1};
+            trace_id(j-1,1) = i;
+            trace_id(j,1) = i+1;
+            sig_all(j-1,1) = sig_ch1;
+            sig_all(j,1) = sig_ch2;
         end 
     end     
 end 
@@ -149,32 +153,31 @@ end
 function traces_out = align_and_center(traces)
 
 limit = length(traces);
-%align each trace using aligntrace
-for (i=1:limit)
-    
+% Aligh pairs of two-color traces
+for i=1:2:limit
     try
-    
-    if ~isempty(traces{i})
-        disp(['rotating trace ' num2str(i) ]);
-        traces{i} = alignTrace_NaNproof_onecolor(traces{i});
-    end
-    
+        if ~isempty(traces{i})
+            disp(['rotating trace ' num2str(i) ]);
+            [traces{i},traces{i+1}] = alignTrace_NaNproof_twocolor(traces{i},traces{i+1});
+        end
     catch %this is if an error occurs, loop is aborted and it continues to next iteration
-    fprintf('Trace rotation messed up');
-    continue;  % Jump to next iteration
+        fprintf('Trace rotation messed up');
+        continue;  % Jump to next iteration
     end %for the try     
 end
 
-%and re-align and minimize each trace
-for (i=1:limit)
-    if ~isempty(traces{i})
+%and re-align and normalize each trace
+for i=1:2:limit
+    if ~isempty(traces{i} && traces{i+1})
         [a b] = polyfit (traces{i}(:,1),(1:1:length(traces{i}(:,1)))',1);
-        if (a(1) < 0);              %if the trces is decreasing (in the x axis), flip to make it increasing
+        if (a(1) < 0);              %if the traces is decreasing (in the x axis), flip to make it increasing
             traces{i} = -traces{i};
         end
-
+        % Normalize trace
         traces{i}(:,1) = traces{i}(:,1) - min(traces{i}(:,1));
-        traces{i}(:,2) = traces{i}(:,2) - min(traces{i}(:,2));   
+        traces{i}(:,2) = traces{i}(:,2) - min(traces{i}(:,2)); 
+        traces{i+1}(:,1) = traces{i+1}(:,1) - min(traces{i+1}(:,1));
+        traces{i+1}(:,2) = traces{i+1}(:,2) - min(traces{i+1}(:,2));
     end
 end
 
